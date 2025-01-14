6fcacc515e1f74fffc33407d12689b24406787db
Benoît Favrie <benoit14.favrie@gmail.com>
2025-01-14 16:01:24 +0100

    feat: add internal transactions ✨

--- src/etherscan/etherscan.service.ts
+++ src/etherscan/etherscan.service.ts
@@ -1,3 +1,4 @@
+// src/etherscan/etherscan.service.ts
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
@@ -25,6 +26,29 @@ export class EtherscanService {
       return data;
     } catch (error) {
       throw new Error(`Failed to fetch transactions: ${error.message}`);
+    }
+  }
+
+  async getInternalTransactions(address: string): Promise<any> {
+    const apiKey = this.configService.get<string>('ETHERSCAN_API_KEY');
+
+    const url = `${this.baseUrl}?module=account&action=txlistinternal&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}`;
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch internal transactions: ${response.statusText}`,
+        );
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error(
+        `Failed to fetch internal transactions: ${error.message}`,
+      );
     }
   }
 }
