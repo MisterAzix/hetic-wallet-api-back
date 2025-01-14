fe795ee41f3bc4ac3f51c5e4eed51e16c538af44
Benoît Favrie <benoit14.favrie@gmail.com>
2025-01-14 14:07:54 +0100

    fix: all tests

--- src/etherscan/etherscan.controller.spec.ts
+++ src/etherscan/etherscan.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
 import { EtherscanController } from './etherscan.controller';
+import { EtherscanService } from './etherscan.service';
 
 describe('EtherscanController', () => {
   let controller: EtherscanController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [EtherscanController],
+      providers: [EtherscanService],
     }).compile();
 
     controller = module.get<EtherscanController>(EtherscanController);
@@ -14,5 +18,12 @@ describe('EtherscanController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return transactions', async () => {
+    const result = await controller.getTransactions(
+      '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae',
+    );
+    expect(result).toHaveProperty('status');
   });
 });
--- src/etherscan/etherscan.service.spec.ts
+++ src/etherscan/etherscan.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
 import { EtherscanService } from './etherscan.service';
 
 describe('EtherscanService', () => {
@@ -6,6 +7,7 @@ describe('EtherscanService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       providers: [EtherscanService],
     }).compile();
 
@@ -14,5 +16,12 @@ describe('EtherscanService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should fetch transactions', async () => {
+    const result = await service.getTransactions(
+      '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae',
+    );
+    expect(result).toHaveProperty('status');
   });
 });
