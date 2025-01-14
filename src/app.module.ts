e141cb2a02188a69629c155cd3fbede13e4b4413
Benoît Favrie <benoit14.favrie@gmail.com>
2025-01-14 14:18:52 +0100

    fix: e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EtherscanModule } from './etherscan/etherscan.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -9,5 +12,7 @@ import { EtherscanModule } from './etherscan/etherscan.module';
     }),
     EtherscanModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
