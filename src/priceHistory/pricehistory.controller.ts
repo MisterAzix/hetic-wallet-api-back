28ac0835150b8467cd35d17fb427155b95380a2f
Charles Lambret <charleslambretpro@gmail.com>
2025-01-15 16:30:43 +0100

    feat: create entries in db with cryptocompare

--- src/priceHistory/pricehistory.controller.ts
+++ src/priceHistory/pricehistory.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { PriceHistoryService } from './pricehistory.service';
-import { Prisma, Symbol } from '@prisma/client';
+import { Prisma, Symbol, Currency } from '@prisma/client';
 import { PriceHistory } from '@prisma/client';
 
 @Controller('pricehistory')
 export class PriceHistoryController {
   constructor(private readonly priceHistoryService: PriceHistoryService) {}
 
-  @Post('/')
-  async create(@Body() createPriceHistoryDto: Prisma.PriceHistoryCreateInput): Promise<PriceHistory> {
-    return this.priceHistoryService.create(createPriceHistoryDto);
-  }
-
   @Get('/:symbol')
   async findBySymbol(@Param('symbol') symbol: Symbol): Promise<PriceHistory[]> {
     return this.priceHistoryService.findBySymbol(symbol);
+  }
+
+  @Post('/create-from-crypto')
+  async createFromCryptoCompare(
+    @Query('symbol') symbol: Symbol,
+    @Query('currency') currency: Currency,
+  ): Promise<PriceHistory> {
+    return this.priceHistoryService.createFromCryptoCompare(symbol, currency);
   }
 }
--- src/priceHistory/pricehistory.service.ts
+++ src/priceHistory/pricehistory.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/PrismaService';
 import { PriceHistory, Prisma, Symbol } from '@prisma/client';
+import { CryptocompareService } from '../cryptocompare/cryptocompare.service';
 
 @Injectable()
 export class PriceHistoryService {
-  constructor(private prisma: PrismaService) {}
-
-  async create(data: Prisma.PriceHistoryCreateInput): Promise<PriceHistory> {
-    return this.prisma.priceHistory.create({ data });
-  }
+  constructor(
+    private prisma: PrismaService,
+    private cryptoService: CryptocompareService,
+  ) {}
 
   async findBySymbol(symbol: Symbol): Promise<PriceHistory[]> {
     return this.prisma.priceHistory.findMany({
       where: { symbol: { equals: symbol } },
     });
+  }
+
+  async createFromCryptoCompare(symbol: Symbol, currency: 'USD'): Promise<PriceHistory> {
+    const priceData = await this.cryptoService.getCryptoPrice(symbol, currency);
+    const price = priceData[currency];
+
+    const priceHistory = await this.prisma.priceHistory.create({
+      data: {
+        symbol,
+        currency,
+        price,
+        date: new Date(),
+      },
+    });
+
+    return priceHistory;
   }
 }
