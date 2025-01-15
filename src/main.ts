00017568f50231cb37a9e48538675a97f4387957
Charles Lambret <charleslambretpro@gmail.com>
2025-01-15 15:55:04 +0100

    fix: add env variable for cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'domaineàchanger.com',
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,POST,PUT,DELETE',
   });
   app.use((req, res, next) => {
