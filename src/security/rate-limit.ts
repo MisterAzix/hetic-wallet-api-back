e3009dcdc35d1938111097619f3a3336c27c0b5c
Charles Lambret <charleslambretpro@gmail.com>
2025-01-15 13:42:50 +0100

    fix: no env variable for rate-limit

--- src/security/rate-limit.ts
+++ src/security/rate-limit.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    ThrottlerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService): ThrottlerModuleOptions => ({
-        ttl: configService.get<number>('THROTTLE_TTL'),
-        limit: configService.get<number>('THROTTLE_LIMIT'),
-      }),
-    }),
+    ThrottlerModule.forRoot([{
+      ttl: 36000,
+      limit: 120,
+    }]),
   ],
 })
 export class RateLimitModule {}
