637b7cd629cc0937054ee9a96001dc52c5281e16
Charles Lambret <79523370+CharlesLambret@users.noreply.github.com>
2025-01-17 09:46:40 +0100

    fix: userID in wallet creation is now a parameter (#24)

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -7,8 +7,8 @@ export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
   @Post('/')
-  async createWallet(@Body() body: { address: string }): Promise<void> {
-    await this.walletService.createWallet(body.address);
+  async createWallet(@Body() body: { address: string, userId: string }): Promise<void> {
+    await this.walletService.createWallet(body.address, body.userId);
   }
 
   @Get('/:address')
